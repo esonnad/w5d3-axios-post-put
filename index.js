b8c6575770d001c4f43b43e4982b0aab365cb60b
Maxence Bouret <maxence.bouret@gmail.com>
2018-11-08 11:02:50 +0100

    Last iteratin

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
-// When the WALL-E button is clicked
-document.getElementById("post-wall-e").onclick = function () {
-  // Create an object with data to submit
-  const characterInfo = {
-    name: 'WALL-E',
-    occupation: 'Waste Allocation Robot',
-    weapon: 'Head laser'
-  };
-  // Make a POST request
-  axios.post('https://ih-crud-api.herokuapp.com/characters', characterInfo)
-    .then(response => {
-      console.log('post success');
-      showFeedback(response)
-    })
-    .catch(error => {
-      console.log('Oh No! Error!');
-      console.log(error)
-    })
-}
+document = document.addEventListener("DOMContentLoaded", function (event) {
 
-// function showFeedback(response) {
-//   const newCharacterHtml =
-//     `
-//       <li>
-//         <h3> ${response.data.name} </h3>
-//         <p> Id: ${response.data.id} </p>
-//       </li>
-//     `;
-//   document.getElementById("characters-list").innerHTML += newCharacterHtml;
-// }
-
-// Create <tr> for all characters in table inside the HTML
-function displayTableRows(characters) {
-  for (let i = 0; i < characters.length; i++) {
-    document.getElementById('tbody').innerHTML += `
+  // Create <tr> for all characters in table inside the HTML
+  function addTableRows(characters) {
+    for (let i = 0; i < characters.length; i++) {
+      document.getElementById('tbody').innerHTML += `
       <tr>
         <td>${characters[i].id}</td>
         <td>${characters[i].name}</td>
         <td>${characters[i].occupation}</td>
         <td>${characters[i].weapon}</td>
       </tr>
-    ` 
+    `
+    }
   }
-}
 
-axios.get('https://ih-crud-api.herokuapp.com/characters')
-.then(response => {
-  displayTableRows(response.data)
-})
+  axios.get('https://ih-crud-api.herokuapp.com/characters')
+    .then(response => {
+      addTableRows(response.data)
+    })
+
+  setTimeout(() => {
+    // When the "add" button is clicked
+    document.getElementById('add-character').onclick = function () {
+      // A character based on the input values is created
+      const characterInfo = {
+        name: document.querySelector('input[name=name]').value,
+        occupation: document.querySelector('input[name=occupation]').value,
+        weapon: document.querySelector('input[name=weapon]').value,
+      };
+      // The character is posted to the API
+      axios.post('https://ih-crud-api.herokuapp.com/characters', characterInfo)
+        .then(response => {
+          // The character is added in the table
+          let characterCreated = response.data
+          addTableRows([characterCreated])
+        })
+        .catch(error => {
+          console.log('Oh No! Error!');
+          console.log(error)
+        })
+    }
+  }, 100)
+});
